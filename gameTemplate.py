e128586cbf07caea6a8cefb89ec083bd28bd3c4b
berkeleyhackclub <44246001+berkeleyhackclub@users.noreply.github.com>
2018-10-17 21:08:58 -0700

    Delete gameTemplate.py

--- gameTemplate.py
+++
@@ -1,8 +0,0 @@
-### GAME TEMPLATE:
-
-while True:
-    input = raw_input("Hey there this is a terminal game template")
-    print(input)
-
-    # IF STATMENTS HERE
-    # GET MORE INPUT IF NECESSARY
